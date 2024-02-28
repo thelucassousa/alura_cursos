d4ca38ffe535c10f5df4fe5687a8366787cbec3e
lucas sousa <luccassous4@outlook.com>
2024-02-28 20:56:18 -0300

    finalizando o código

--- javascript-web-paginas-dinamicas/js/main.js
+++ javascript-web-paginas-dinamicas/js/main.js
@@ -1,14 +1,26 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (idElementoAudio) { //está função serve para quando iniciar o site, já não começar tocando, mas esperar a interação do usuário para reproduzir algo.
     document.querySelector(idElementoAudio).play();
 };
 
-const ListaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0
+const ListaDeTeclas = document.querySelectorAll('.tecla'); //selecionar todas as teclas.
 
 
-while(contador < ListaDeTeclas.length ){
-    ListaDeTeclas[contador].onclick = tocaSom;
-    contador ++;
-    console.log(contador)
+for (let contador = 0; contador < ListaDeTeclas.length; contador++){
+
+    const tecla = ListaDeTeclas[contador]; //me diz a referencia da tecla
+    const instrumento = tecla.classList[1]; //pega a referência da tecla e me retorna o segundo nome da tag no html
+    const idAudio = `#som_${instrumento}`; //concatenando o "#som" com a classe 
+
+    tecla.onclick = function (){ //função oculta
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
 }
